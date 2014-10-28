1e7c7aa1170519b691e2a9a75769b2a1d61062e9
FrbezarN <nicolas.frbezar@gmail.com>
2014-10-28 21:03:06 +0100

    Recupératon de l'equation par une chaine de caractère

--- AlgoA/main.c
+++ AlgoA/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void saisiea(char *t)
+{
+    int i ;
+    char car;
+    i=0;
+    do
+        {
+        car=getchar(); //getchar , code asci de ce qui est prit au clavier
+        t[i]=car; //peut placer directement t[i]=getchar ....
+        i++;
+        }
+    while(car!='\n'); //.... remplacer car par t[i-1]
+    t[i-1]='\0'; // Pour degager le \n
+}
+
+//Partie a 2eme point , on met dans un tableau ajuster
+char *saisieb()
+{
+    int taille ;
+    char *d , tmp[100];
+    saisiea(tmp);//A
+    taille=strlen(tmp)+1;//B
+    d=(char*)malloc(taille*sizeof(char));
+    strcpy(d,tmp);
+    return d;
+}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char *s;
+    s = saisieb();
+    printf("%s",s);
 }
